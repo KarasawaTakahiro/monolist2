ecc0a285b2f8de8cb202f8e691f1662a531617b3
tkhr <karasawa@basicinc.jp>
2016-05-20 01:14:50 +0000

    new item page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  
+  helper_method :mediumImage_to_largeImage
+
+  # midiumImageをLargeImageに変換する
+  def mediumImage_to_largeImage(img_url)
+    img_url.gsub('?_ex=128x128', '')
+  end
+
+
   private
   def logged_in_user
     unless logged_in?
